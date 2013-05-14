2bb495255f28128622cac0432745dc2f9153211f
Chris Maxwell <cmaxwell@ojala.com>
2013-05-14 12:50:59 -0400

    Initial commit. Install Refinery.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Champion::Application.config.secret_token = 'bfb77df1887bc75b5f466e1dd204526fda55d66ea15d70025a164bee524424d077d72025900bed8339671caba6fa011aeeecfbc4263ab040f53e1b91fdd31f97'
